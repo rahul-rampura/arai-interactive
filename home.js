49c669cba91488b05468f9810051877c4dc3511c
rahul-rampura <rahul.rampura@gmail.com>
2016-12-31 17:22:36 +0530

    Added launch video on clicking fullscreen button

--- home.js
+++ home.js
@@ -4,6 +4,23 @@
     var fid_btn = document.getElementById('fid_btn');
     var exit_btn = document.getElementById('exit_btn');
 
+    //Handle Kothrud
+    kothrud_btn.addEventListener('click', function(){
+        location.href = "kothrud.html";
+    });
+
+    //Handle HTC
+    htc_btn.addEventListener('click', function(){
+        location.href = "htc.html";
+    });
+
+    //Handle FID
     fid_btn.addEventListener('click', function(){
         location.href = "fid.html";
+    });
+
+    //Handle exit
+    exit_btn.addEventListener('click', function(){
+        window.opener = null;
+        return false;
     });
--- launch.js
+++ launch.js
@@ -1,18 +1,35 @@
 
     var fs_btn = document.getElementById('fs_btn');
     fs_btn.addEventListener('click', function() {
-      if (document.requestFullscreen) {
-        console.log('Fullscreen1');
-        document.requestFullscreen();
-      } else if (document.msRequestFullscreen) {
-        console.log('Fullscreen2');
-        document.msRequestFullscreen();
-      } else if (document.mozRequestFullScreen) {
-        console.log('Fullscreen3');
-        document.mozRequestFullScreen();
-      } else if (document.webkitRequestFullscreen) {
-        console.log('Fullscreen4');
-        document.webkitRequestFullscreen();
-      }
-        location.href = "home.html";
+        //Embedding launch video
+        var vid = document.createElement('video');
+        vid.id = "launch_video";
+        vid.controls = "controls";
+        vid.src = "arai_logo.mp4";
+        document.getElementsByTagName('body')[0].appendChild(vid);
+
+        //Play launch video
+        var lvid = document.getElementById("launch_video");
+        document.getElementById('container').style.visibility = 'hidden';
+        lvid.play();
+
+        //Apply Fullscreen
+        if (lvid.requestFullscreen) {
+          console.log('Fullscreen1');
+          lvid.requestFullscreen();
+        } else if (lvid.msRequestFullscreen) {
+          console.log('Fullscreen2');
+          lvid.msRequestFullscreen();
+        } else if (lvid.mozRequestFullScreen) {
+          console.log('Fullscreen3');
+          lvid.mozRequestFullScreen();
+        } else if (lvid.webkitRequestFullscreen) {
+          console.log('Fullscreen4');
+          lvid.webkitRequestFullscreen();
+        }
+        
+        //Redirect to home page after video ends
+        lvid.addEventListener('ended', function() {
+          location.href = "home.html";
+        });
     });
