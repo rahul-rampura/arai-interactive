e43f8238b70e9f9fe1e167bfc320296206b76a5a
rahul-rampura <rahul.rampura@gmail.com>
2016-12-31 15:45:19 +0530

    Added buttons on home page at proper positions

---
+++ home.js
@@ -0,0 +1,9 @@
+
+    var kothrud_btn = document.getElementById('kothrud_btn');
+    var htc_btn = document.getElementById('htc_btn');
+    var fid_btn = document.getElementById('fid_btn');
+    var exit_btn = document.getElementById('exit_btn');
+
+    fid_btn.addEventListener('click', function(){
+        location.href = "fid.html";
+    });
--- launch.js
+++ launch.js
@@ -1,5 +1,18 @@
 
     var fs_btn = document.getElementById('fs_btn');
     fs_btn.addEventListener('click', function() {
+      if (document.requestFullscreen) {
+        console.log('Fullscreen1');
+        document.requestFullscreen();
+      } else if (document.msRequestFullscreen) {
+        console.log('Fullscreen2');
+        document.msRequestFullscreen();
+      } else if (document.mozRequestFullScreen) {
+        console.log('Fullscreen3');
+        document.mozRequestFullScreen();
+      } else if (document.webkitRequestFullscreen) {
+        console.log('Fullscreen4');
+        document.webkitRequestFullscreen();
+      }
         location.href = "home.html";
     });
